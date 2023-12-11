97510ec054ac3c10b417469bb341f48a0d1bdbf8
Pavel Voronin <pavel@mediabuyingsystems.com>
2023-12-11 12:25:15 +0100

    Add project files.

---
+++ ValueConverterIsNotUsedInExecuteSql/Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+var ctx = new AppDbContext();
+
+var date = new DateIndex(20231211);
+
+
+ctx.Database.ExecuteSql($"""UPDATE Entity SET "Date"={date};"""); // THROWS
+
+
+public readonly record struct DateIndex
+{
+    public readonly int Value;
+
+    public DateIndex(int value)
+    {
+        Value = value;
+    }
+}
+
+public sealed class Entity
+{
+    public int Id { get; private set; }
+    public DateIndex Date { get; set; }
+}
+
+public sealed class AppDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("DataSource=:memory:;");
+    }
+
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        configurationBuilder.Properties<DateIndex>(_ =>
+            {
+                _.HaveConversion<DateIndexToIntConverter>();
+            });
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Entity>();
+    }
+
+    private sealed class DateIndexToIntConverter : ValueConverter<DateIndex, int>
+    {
+        public DateIndexToIntConverter()
+            : base(model => model.Value, dbValue => new DateIndex(dbValue), convertsNulls: false)
+        {
+        }
+    }
+}
